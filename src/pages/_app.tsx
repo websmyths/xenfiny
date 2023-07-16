b1b70129284f0f2401cb34f25f8feadbb879c3ae
Grokgpt <grok@grokgpt.com>
2023-07-16 16:17:57 -0500

    Added Mantine UI library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,22 @@ import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 import { api } from "~/utils/api";
+import { MantineProvider } from '@mantine/core';
 import "~/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </ClerkProvider>
   );
 }
---
+++ src/pages/_document.tsx
@@ -0,0 +1,35 @@
+import { createGetInitialProps } from '@mantine/next';
+import Document, { Head, Html, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyles, createStylesServer } from '@mantine/next';
+
+const getInitialProps = createGetInitialProps();
+// optional: you can provide your cache as a first argument in createStylesServer function
+const stylesServer = createStylesServer();
+
+export default class _Document extends Document {
+    static async getInitialProps(ctx: any) {
+        const initialProps = await Document.getInitialProps(ctx);
+
+        // Add your app specific logic here
+
+        return {
+            ...initialProps,
+            styles: [
+                initialProps.styles,
+                <ServerStyles html={initialProps.html} server={stylesServer} key="styles" />,
+            ],
+        };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
